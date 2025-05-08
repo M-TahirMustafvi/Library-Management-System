c3c8b698a8ce045b83993e478a473e2742379f35
Tahir Mustafvi <tahirmustafvi215@proton.me>
2025-05-08 18:24:34 +0500

    LMS

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import './App.css'
+
+const router = createBrowserRouter([
+
+])
+
+function App() {
+  
+  return (
+    <>
+      <p>This will be your website</p>
+    </>
+  )
+}
+
+export default App
